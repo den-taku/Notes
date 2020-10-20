38005a78b5bae798ee090d8641d73ca609a1fb56
DenTaku <dentaku1412@gmail.com>
2020-10-21 00:08:32 +0900

    Done function

--- plotlib/plotlib/src/main.rs
+++ plotlib/plotlib/src/main.rs
@@ -1,22 +1,17 @@
 use plotlib::page::Page;
-use plotlib::repr::BoxPlot;
-use plotlib::style::BoxStyle;
-use plotlib::view::CategoricalView;
+use plotlib::repr::Plot;
+use plotlib::style::LineStyle;
+use plotlib::view::ContinuousView;
 
 fn main() {
-    let b1 = BoxPlot::from_slice(&[1.0, 4.0, 2.0, 3.5, 6.4, 2.5, 7.5, 1.8, 9.6]).label("1");
-    let b2 = BoxPlot::from_slice(&[3.0, 4.3, 2.0, 3.5, 6.9, 4.5, 7.5, 1.8, 10.6])
-        .label("2")
-        .style(&BoxStyle::new().fill("darkolivegreen"));
+    let f1 =
+        Plot::from_function(|x| x * 5., 0., 10.).line_style(LineStyle::new().colour("burlywood"));
+    let f2 = Plot::from_function(|x| x.powi(2), 0., 10.)
+        .line_style(LineStyle::new().colour("darkolivegreen").width(2.));
+    let f3 = Plot::from_function(|x| x.sqrt() * 20., 0., 10.)
+        .line_style(LineStyle::new().colour("brown").width(1.));
 
-    let v = CategoricalView::new()
-        .add(b1)
-        .add(b2)
-        .x_label("Experiment")
-        .y_label("y");
+    let v = ContinuousView::new().add(f1).add(f2).add(f3);
 
-    Page::single(&v)
-        .dimensions(400, 300)
-        .save("boxplot.svg")
-        .expect("saving svg");
+    Page::single(&v).save("function.svg").expect("saving svg");
 }
