7131dab7697d323edce74b2265815537c9c11441
DenTaku <dentaku1412@gmail.com>
2020-10-21 00:28:22 +0900

    Done line_and_point

--- plotlib/plotlib/src/main.rs
+++ plotlib/plotlib/src/main.rs
@@ -1,30 +1,19 @@
-use std::collections::btree_map::BTreeMap;
+use plotlib::page::Page;
+use plotlib::repr::Plot;
+use plotlib::style::*;
+use plotlib::view::ContinuousView;
 
 fn main() {
-    let mut data = Vec::new();
-    let message: &str = "This is a long message";
-    let mut count = BTreeMap::new();
+    let l1 = Plot::new(vec![(0., 1.), (2., 1.5), (3., 1.2), (4., 1.1)])
+        .line_style(
+            LineStyle::new()
+                .colour("burlywood")
+                .linejoin(LineJoin::Round),
+        )
+        .point_style(PointStyle::new());
 
-    for c in message.trim().to_lowercase().chars() {
-        if c.is_alphabetic() {
-            *count.entry(c).or_insert(0) += 1
-        }
-    }
-
-    println!("Number of occurences per character");
-    for (ch, count) in &count {
-        println!("{:?}: {}", ch, count);
-        let count = *count as f64;
-        data.push(plotlib::repr::BarChart::new(count).label(ch.to_string()));
-    }
-    // Add data to the view
-    let v = data
-        .into_iter()
-        .fold(plotlib::view::CategoricalView::new(), |view, datum| {
-            view.add(datum)
-        });
-
-    plotlib::page::Page::single(&v)
-        .save("barchart.svg")
+    let v = ContinuousView::new().add(l1);
+    Page::single(&v)
+        .save("line_and_point.svg")
         .expect("saving svg");
 }
