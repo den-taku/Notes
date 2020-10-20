5d0463e00a29a48da30ef62c88295a88b66f1430
DenTaku <dentaku1412@gmail.com>
2020-10-21 00:35:10 +0900

    Done scatter_text

--- plotlib/plotlib/src/main.rs
+++ plotlib/plotlib/src/main.rs
@@ -4,8 +4,7 @@ use plotlib::style::{PointMarker, PointStyle};
 use plotlib::view::ContinuousView;
 
 fn main() {
-    // Scatter plots expect a list of pairs
-    let data1 = vec![
+    let data = vec![
         (-3.0, 2.3),
         (-1.6, 5.3),
         (0.3, 0.7),
@@ -13,22 +12,10 @@ fn main() {
         (6.4, 4.3),
         (8.5, 3.7),
     ];
+    let s1 = Plot::new(data).point_style(PointStyle::new().marker(PointMarker::Circle));
+    let s2 = Plot::new(vec![(-1.4, 2.5), (7.2, -0.3)])
+        .point_style(PointStyle::new().marker(PointMarker::Square));
 
-    // We create our scatter plot from the data
-    let s1: Plot = Plot::new(data1).point_style(
-        PointStyle::new()
-            .marker(PointMarker::Square) // setting the marker to be a square
-            .colour("#DD3355"),
-    ); // and a custom colour
-
-    // We can plot multiple data sets in the same view
-    let data2 = vec![(-1.4, 2.5), (7.2, -0.3)];
-    let s2: Plot = Plot::new(data2).point_style(
-        PointStyle::new() // uses the default marker
-            .colour("#35C788"),
-    ); // and a different colour
-
-    // The 'view' describes what set of data is drawn
     let v = ContinuousView::new()
         .add(s1)
         .add(s2)
@@ -37,6 +24,5 @@ fn main() {
         .x_label("Some varying variable")
         .y_label("The response of something");
 
-    // A page with a single view is then saved to an SVG file
-    Page::single(&v).save("scatter.svg").unwrap();
+    println!("{}", Page::single(&v).dimensions(80, 30).to_text().unwrap());
 }
