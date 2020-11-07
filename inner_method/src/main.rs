906129d91c653b3fc9c45aee128818440d972389
DenTaku <dentaku1412@gmail.com>
2020-11-07 22:17:45 +0900

    Comfirm mut mut

--- inner_method/src/main.rs
+++ inner_method/src/main.rs
@@ -1,3 +1,22 @@
+#[derive(Debug, Copy, Clone)]
+struct Point {
+    x: f32,
+    y: f32
+}
+
+impl Point {
+    fn inner(&mut self) {
+        self.x += 1.0;
+        self.y += 1.0;
+    }
+    fn inner_wraper(&mut self) {
+        self.inner()
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut point = Point { x: 3.0, y: 4.0 };
+    println!("{:?}", point);
+    point.inner_wraper();
+    println!("{:?}", point);
 }
