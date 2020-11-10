501665ce7be72fde225d3c7439b91f20dfdf42b2
DenTaku <dentaku1412@gmail.com>
2020-11-11 01:00:43 +0900

    Check relationship with array and memory

--- array_memory/src/main.rs
+++ array_memory/src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    let hoge1 = [true; 16384];
+    let hoge2 = [false; 16834];
+    for i in 0..16384 {
+        println!("{}", hoge1[i]);
+        println!("{}", hoge2[i]);
+    }
 }
