a0f189bdea75c318a8646b4fd94cc0a414f6d163
DenTaku <dentaku1412@gmail.com>
2020-11-26 17:35:46 +0900

    Learn about tuple

--- file_input/target/debug/deps/file_input-42a32e90c814fbb4.d
+++
@@ -1,5 +0,0 @@
-/Users/tatanbo/dentaku/notes/file_input/target/debug/deps/file_input-42a32e90c814fbb4.rmeta: src/main.rs
-
-/Users/tatanbo/dentaku/notes/file_input/target/debug/deps/file_input-42a32e90c814fbb4.d: src/main.rs
-
-src/main.rs:
--- file_input/target/rls/debug/deps/file_input-70939414dc0df688.d
+++
@@ -1,5 +0,0 @@
-/Users/tatanbo/dentaku/notes/file_input/target/rls/debug/deps/file_input-70939414dc0df688.rmeta: src/main.rs
-
-/Users/tatanbo/dentaku/notes/file_input/target/rls/debug/deps/file_input-70939414dc0df688.d: src/main.rs
-
-src/main.rs:
