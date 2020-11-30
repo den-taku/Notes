5f9084e5a65f3077ff1296fd5663a33fdb6360c2
DenTaku <dentaku1412@gmail.com>
2020-11-30 19:08:39 +0900

    Review if

---
+++ reduction_if/src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    let condition = false;
+    if if if if condition { false } else { true } {
+        true
+    } else {
+        false
+    } {
+        true
+    } else {
+        false
+    } {
+        println!("Great!");
+    } else {
+        println!("bad...");
+    }
+    println!("Hello, world!");
+}
