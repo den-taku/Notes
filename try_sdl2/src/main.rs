4b4e4bc40eed64fae17c71cfe207de2c9bf7cebe
DenTaku <dentaku1412@gmail.com>
2020-12-10 17:41:05 +0900

    Reading https://sunjay.dev/learn-game-dev/refactor-traditional-game-loop.html

--- try_sdl2/src/main.rs
+++ try_sdl2/src/main.rs
@@ -2,27 +2,33 @@ use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use std::time::Duration;
+use sdl2::render::WindowCanvas;
+
+fn render(canvas: &mut WindowCanvas, color: Color) {
+    canvas.set_draw_color(color);
+    canvas.clear();
+    canvas.present();
+}
  
-pub fn main() {
-    let sdl_context = sdl2::init().unwrap();
-    let video_subsystem = sdl_context.video().unwrap();
+fn main() -> Result<(), String> {
+    let sdl_context = sdl2::init()?;
+    let video_subsystem = sdl_context.video()?;
  
     let window = video_subsystem.window("Screen", 512, 256)
         .position_centered()
         .build()
-        .unwrap();
+        .expect("could not initialize video subsystem");
  
-    let mut canvas = window.into_canvas().build().unwrap();
+    let mut canvas = window.into_canvas().build()
+        .expect("could not make a canvas");
  
-    canvas.set_draw_color(Color::RGB(255, 255, 255));
-    canvas.clear();
-    canvas.present();
-    let mut event_pump = sdl_context.event_pump().unwrap();
+    // canvas.set_draw_color(Color::RGB(255, 255, 255));
+    // canvas.clear();
+    // canvas.present();
+    let mut event_pump = sdl_context.event_pump()?;
     let mut i = 0;
     'running: loop {
-        i = (i + 1) % 255;
-        canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
-        canvas.clear();
+        // Handle events
         for event in event_pump.poll_iter() {
             match event {
                 Event::Quit {..} |
@@ -32,9 +38,15 @@ pub fn main() {
                 _ => {}
             }
         }
-        // The rest of the game loop goes here...
 
-        canvas.present();
+        // Update
+        i = (i + 1) % 255;
+
+        // Render
+        render(&mut canvas, Color::RGB(i, 64, 255 - i));
+
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
+
+    Ok(())
 }
