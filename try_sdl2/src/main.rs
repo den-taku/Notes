0be96031525e21b42598777241138c1f7a7a924d
DenTaku <dentaku1412@gmail.com>
2020-12-10 17:17:54 +0900

    Modify for Screen

--- try_sdl2/src/main.rs
+++ try_sdl2/src/main.rs
@@ -7,14 +7,14 @@ pub fn main() {
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
  
-    let window = video_subsystem.window("rust-sdl2 demo", 800, 600)
+    let window = video_subsystem.window("Screen", 512, 256)
         .position_centered()
         .build()
         .unwrap();
  
     let mut canvas = window.into_canvas().build().unwrap();
  
-    canvas.set_draw_color(Color::RGB(0, 255, 255));
+    canvas.set_draw_color(Color::RGB(255, 255, 255));
     canvas.clear();
     canvas.present();
     let mut event_pump = sdl_context.event_pump().unwrap();
