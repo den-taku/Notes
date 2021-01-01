6e0ae8b5493e8759d1a1af74c63a736fdabf66af
DenTaku <dentaku1412@gmail.com>
2021-01-02 03:54:51 +0900

    Learn asink

--- asink/src/main.rs
+++ asink/src/main.rs
@@ -1,3 +1,26 @@
+use asink::*;
+use std::sync::mpsc::Sender;
+
+/// This will be serialized into msgpack
+#[derive(Serialize)]
+struct Doc {
+    id: usize,
+    data: Vec<f64>,
+}
+
+fn experiment(s: Sender<Doc>) {
+    for i in 0..5 {
+        let doc = Doc {
+            id: i,
+            data: vec![i as f64],
+        };
+        s.send(doc).unwrap(); // Send data to sink
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let sink = msgpack::MsgpackSink::from_str("test.msg");
+    let (s, th) = sink.run(); // Sink start thread to write recieved data into msgpack
+    experiment(s);
+    th.join().unwrap();
 }
