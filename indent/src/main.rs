ef5d102212454d094548107bd1060cdc99f7ebd6
DenTaku <dentaku1412@gmail.com>
2021-03-02 22:48:59 +0900

    Check indent

---
+++ indent/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    println!("Hello, world!");
+
+    if true {
+        println!("true")
+    } else if false {
+        println!("false")
+    }
+
+    if true {
+        println!("true")
+    } else if false {
+        println!("false")
+    }
+}
