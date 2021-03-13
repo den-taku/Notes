7ac3d9bff10714cba24b3af4d18326ee00e6ce46
DenTaku <dentaku1412@gmail.com>
2021-03-13 22:55:07 +0900

    Finish

--- equal/src/main.rs
+++ equal/src/main.rs
@@ -1,4 +1,15 @@
 fn main() {
     println!("{}", (3 < 4) == true);
+    // println!("{}", Hoge{} == true);
     println!("Hello, world!");
 }
+
+// struct Hoge{}
+
+// impl PartialEq<bool> for Hoge 
+// {
+//     fn eq(&self, other: &bool) -> bool {
+//         !other
+//     }
+// }
+
