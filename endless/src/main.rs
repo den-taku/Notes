3d8dd204d91d15e67c783d58a8bfaa3bf01e1c66
DenTaku <dentaku1412@gmail.com>
2021-03-19 11:59:23 +0900

    Experiment infinite recrusive

--- endless/src/main.rs
+++ endless/src/main.rs
@@ -2,6 +2,29 @@ fn f(x: i32) -> i32 {
     f(x)
 }
 
+fn g(x: f32) -> f32 {
+    g(x)
+}
+
+fn h(x: i32) -> f32 {
+    h(x)
+}
+
+fn i(x: f32) -> i32 {
+    i(x)
+}
+
 fn main() {
-    f(4);
+    for n in 0..=10 {
+        dbg!(f(n));
+    }
+    for r in 0..=10 {
+        dbg!(g(r as f32));
+    }
+    for n in 0..=10 {
+        dbg!(h(n));
+    }
+    for r in 0..=10 {
+        dbg!(i(r as f32));
+    }
 }
