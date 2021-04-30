dd76c562ba19a4b55176ae0829fa240a7a56e718
DenTaku <dentaku1412@gmail.com>
2021-05-01 00:09:30 +0900

    Add another target

---
+++ helloworld/main/hellogreet.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+void greet() {
+    std::cout << "Sun Arg  6 15:23:47 2021" << std::endl;
+}
--- helloworld/main/helloworld.cpp
+++ helloworld/main/helloworld.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include "hellogreet.h"
 
 int main(void) {
     std::cout << "Hello World!!" << std::endl;
+    greet();
     return 0;
 }
